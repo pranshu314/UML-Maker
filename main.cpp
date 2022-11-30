734d3a3c2de731424e5791453fc9486c6ec3bfa5
Pranshu Goyal <pranshu2.71828@gmail.com>
2022-11-30 16:02:51 +0530

    Changed main.cpp so a new directory is created for storing class files and they also detect function now

--- main.cpp
+++ main.cpp
@@ -153,7 +153,8 @@ class IndividualClassParser
     private:
         void singleClassFileGenerator(string class_name, string singleFileName)
         {
-            cout<<"\n1\n";
+            system("mkdir uml_files");
+            //cout<<"\n1\n";
             cout<<singleFileName;
             cout<<"\n";
             regex derived_regex(".*<derivedcompoundref refid=.*");
@@ -173,11 +174,11 @@ class IndividualClassParser
                 {
                     if(regex_match(line,derived_regex))
                     {
-                        cout<<"\n3\n";
+                        //cout<<"\n3\n";
                         smatch match_flag;
                         regex_search(line,match_flag,derived_name_regex);
                         ofstream inheritance_file;
-                        inheritance_file.open("inheritance.txt", ios_base::app);
+                        inheritance_file.open("uml_files/inheritance.txt", ios_base::app);
                         inheritance_file<<class_name<<" : ";
                         for(auto x : match_flag)
                         {
@@ -186,7 +187,7 @@ class IndividualClassParser
                     }
                     if(regex_match(line,variable_regex))
                     {
-                        cout<<"\n4\n";
+                        //cout<<"\n4\n";
                         string tmp_line;
                         string prot;
                         string _static;
@@ -247,14 +248,148 @@ class IndividualClassParser
                         string tmp_space=" ";
                         string final_member_name = var+prot+tmp_colon+name+tmp_semicolon+_static+tmp_space+type;
 
-                        ofstream single_class_file;
-                        string file_name=class_name+".txt";
-                        single_class_file.open(file_name, ios_base::app);
-                        single_class_file<<final_member_name<<endl;
+                        int copy_check=0;
+                        ifstream copy_file;
+                        copy_file.open("uml_files/"+class_name+".txt");
+                        while(copy_file)
+                        {
+                            string line_from_file;
+                            getline(copy_file,line_from_file);
+                            if(line_from_file==final_member_name)
+                            {
+                                copy_check+=1;
+                            }
+                            else if(line=="-1")
+                            {
+                                break;
+                            }
+                        }
+                        
+                        if(copy_check==0)
+                        {
+                            ofstream single_class_file;
+                            string file_name="uml_files/"+class_name+".txt";
+                            single_class_file.open(file_name, ios_base::app);
+                            single_class_file<<final_member_name<<endl;
+                        }
+
                     }
                     if(regex_match(line,function_regex))
                     {
-                        cout<<"\n5\n";
+                        //cout<<"\n5\n";
+                        string tmp_line;
+                        string prot;
+                        string _static;
+                        string constant;
+                        string virt;
+                        string type;
+                        string argsstring;
+                        string name;
+
+                        size_t found_prot = line.find("prot=");
+                        if(line.substr(int(found_prot)+6,6)=="public")
+                        {
+                            prot="public";
+                        }
+                        else if(line.substr(int(found_prot)+6,7)=="private")
+                        {
+                            prot="private";
+                        }
+                        else if(line.substr(int(found_prot)+6,9)=="protected")
+                        {
+                            prot="protected";
+                        }
+
+                        size_t found_static = line.find("static=");
+                        if(line.substr(int(found_static)+8,3)=="yes")
+                        {
+                            _static="static";
+                        }
+                        else if(line.substr(int(found_static)+8,2)=="no")
+                        {
+                            _static="";
+                        }
+
+                        size_t found_const = line.find("const=");
+                        if(line.substr(int(found_const)+7,3)=="yes")
+                        {
+                            constant="const";
+                        }
+                        else if(line.substr(int(found_const)+7,2)=="no")
+                        {
+                            constant="";
+                        }
+
+                        size_t found_virtual = line.find("virt=");
+                        if(line.substr(int(found_virtual)+6,11)=="non-virtual")
+                        {
+                            virt="";
+                        }
+                        else if(line.substr(int(found_virtual)+6,7)=="virtual")
+                        {
+                            virt="virtual";
+                        }
+
+                        regex type_regex(".*<type>.*</type>");
+                        regex argsstring_regex(".*<argsstring>.*</argsstring>");
+                        regex name_regex(".*<name>.*</name>");
+                        getline(work_file,tmp_line);
+                        if(regex_match(tmp_line,type_regex))
+                        {
+                            regex type_2_regex(".*<type><ref .*>.*</ref></type>");
+                            if(regex_match(tmp_line,type_2_regex))
+                            {
+                                size_t found_type = tmp_line.find(">");
+                                found_type=tmp_line.find(">",found_type+1);
+                                type=tmp_line.substr(int(found_type)+1,tmp_line.length()-13-int(found_type)-1);
+                            }
+                            else
+                            {
+                                type=tmp_line.substr(14,tmp_line.length()-21);
+                            }
+                        }
+                        getline(work_file,tmp_line);
+                        getline(work_file,tmp_line);
+                        if(regex_match(tmp_line,argsstring_regex))
+                        {
+                            argsstring=tmp_line.substr(20,tmp_line.length()-33);
+                        }
+                        getline(work_file,tmp_line);
+                        if(regex_match(tmp_line,name_regex))
+                        {
+                            name=tmp_line.substr(14,tmp_line.length()-21);
+                        }
+
+                        string func="function :: ";
+                        string tmp_colon=" : ";
+                        string tmp_semicolon=" ; ";
+                        string tmp_space=" ";
+                        string final_member_name=func+prot+tmp_colon+name+tmp_semicolon+virt+tmp_space+_static+tmp_space+constant+tmp_space+type;
+
+                        int copy_check=0;
+                        ifstream copy_file;
+                        copy_file.open("uml_files/"+class_name+".txt");
+                        while(copy_file)
+                        {
+                            string line_from_file;
+                            getline(copy_file,line_from_file);
+                            if(line_from_file==final_member_name)
+                            {
+                                copy_check+=1;
+                            }
+                            else if(line=="-1")
+                            {
+                                break;
+                            }
+                        }
+                        
+                        if(copy_check==0)
+                        {
+                            ofstream single_class_file;
+                            string file_name="uml_files/"+class_name+".txt";
+                            single_class_file.open(file_name, ios_base::app);
+                            single_class_file<<final_member_name<<endl;
+                        }
                     }
 
                     line2=line;
